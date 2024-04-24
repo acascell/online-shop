ac3729fc1ce8660837b0e8cb97152b2416eddeb7
acascell <infocascella@gmail.com>
2024-04-24 11:32:07 +0200

    feat: celery task to offload confirmation email taks
    - defined new celery task which takes care of sending emails to the user after an order has been completed
    - used celery flower to monitor task execution and rabbit mq as message broker
    - updated order view to generate a task after order completion

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderForm
 from cart.cart import Cart
+from .tasks import order_created
+
 
 # Create your views here.
 
@@ -21,6 +23,8 @@ def order_create(request):
                 )
 
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderForm()
