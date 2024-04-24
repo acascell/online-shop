2448ce83e4a35ff4038fa9e9190336342d7fdbe5
acascell <infocascella@gmail.com>
2024-04-24 12:53:23 +0200

    fix: included order celery task

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -26,6 +26,7 @@ def order_create(request):
 
             cart.clear()
             # launch asynchronous task
+            order_created.delay(order.id)
             # set the order in the session
             request.session["order_id"] = order.id
             # redirect for payment
