52aad911fca0069c250b17cd4404327905561610
acascell <infocascella@gmail.com>
2024-04-25 10:11:46 +0200

    feat: registered order payment within admin app

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,10 +1,26 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ""
+
+
+order_payment.short_description = "Stripe payment"
 
 
 @admin.register(Order)
@@ -18,6 +34,7 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
+        order_payment,
         "created",
         "updated",
     ]
