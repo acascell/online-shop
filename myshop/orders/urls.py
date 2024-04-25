b8098501a8ab01d3c61d5fe01cc5d5ae0c052f58
acascell <infocascella@gmail.com>
2024-04-25 14:57:17 +0200

    feat: implemented generate pdf function
    - defined logic to convert html to pdf
    - defined endpoint
    - updated models
    - created shared task to use within the webhook after the payment has been correctly registered
    - updated settings using static root

--- myshop/orders/urls.py
+++ myshop/orders/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 app_name = "orders"
+
 
 urlpatterns = [
     path("create/", views.order_create, name="order_create"),
@@ -10,4 +12,7 @@ urlpatterns = [
         views.admin_order_detail,
         name="admin_order_detail",
     ),
+    # path('admin/order/<int:order_id>/pdf/',
+    #      views.admin_order_pdf,
+    #      name='admin_order_pdf'),
 ]
